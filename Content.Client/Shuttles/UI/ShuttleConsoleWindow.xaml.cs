07d869e69b9ea237c2de9c82a1f2ea808224830c
Shawn McHenry <drraccoony@gmail.com>
2026-01-20 20:51:45 -0600

    shuttle auto merge 2

--- Content.Client/Shuttles/UI/ShuttleConsoleWindow.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleConsoleWindow.xaml.cs
@@ -71,7 +71,13 @@ public sealed partial class ShuttleConsoleWindow : FancyWindow,
             UndockRequest?.Invoke(entity);
         };
 
+        DockContainer.UndockAllRequest += dockEntities =>
+        {
+            UndockAllRequest?.Invoke(dockEntities);
+        };
+
         NfInitialize(); // Frontier Initialization for the ShuttleConsoleWindow
+        WfInitialize(); // Wayfarer Initialization for the ShuttleConsoleWindow
     }
 
     private void ClearModes(ShuttleConsoleMode mode)
